9af7a9ba33f6d2457acd9f4c45cee498b6309610
Adam Taras <ataras2@gmail.com>
2022-12-15 15:32:40 +1100

    trying to make flows init nicely with gatedConv issue

--- sibylla/Norm_Flows/flows.py
+++ sibylla/Norm_Flows/flows.py
@@ -8,65 +8,6 @@ import jax.numpy as np
 
 from typing import Sequence
 from flax import linen as nn
+from layers import *
 
 
-# ------------------------------- Base flow -------------------------------
-
-class ImageFlow(nn.Module):
-    flows: Sequence[nn.Module]  # A list of flows (each a nn.Module) that should be applied on the images.
-    import_samples: int = 8  # Number of importance samples to use during testing (see explanation below).
-
-    def __call__(self, x, rng, testing=False):
-        if not testing:
-            bpd, rng = self._get_likelihood(x, rng)
-        else:
-            # Perform importance sampling during testing => estimate likelihood M times for each image
-            img_ll, rng = self._get_likelihood(x.repeat(self.import_samples, 0), rng, return_ll=True)
-            img_ll = img_ll.reshape(-1, self.import_samples)
-
-            # To average the probabilities, we need to go from log-space to exp, and back to log.
-            # Logsumexp provides us a stable implementation for this
-            img_ll = jax.nn.logsumexp(img_ll, axis=-1) - onp.log(self.import_samples)
-
-            # Calculate final bpd
-            bpd = -img_ll * onp.log2(onp.exp(1)) / onp.prod(x.shape[1:])
-            bpd = bpd.mean()
-        return bpd, rng
-
-    def encode(self, imgs, rng):
-        # Given a batch of images, return the latent representation z and ldj of the transformations
-        z, ldj = imgs, np.zeros(imgs.shape[0])
-        for flow in self.flows:
-            z, ldj, rng = flow(z, ldj, rng, reverse=False)
-        return z, ldj, rng
-
-    def _get_likelihood(self, imgs, rng, return_ll=False):
-        """
-        Given a batch of images, return the likelihood of those.
-        If return_ll is True, this function returns the log likelihood of the input.
-        Otherwise, the ouptut metric is bits per dimension (scaled negative log likelihood)
-        """
-        z, ldj, rng = self.encode(imgs, rng)
-        log_pz = jax.scipy.stats.norm.logpdf(z).sum(axis=(1, 2, 3))
-        log_px = ldj + log_pz
-        nll = -log_px
-        # Calculating bits per dimension
-        bpd = nll * onp.log2(onp.exp(1)) / onp.prod(imgs.shape[1:])
-        return (bpd.mean() if not return_ll else log_px), rng
-
-    def sample(self, img_shape, rng, z_init=None):
-        """
-        Sample a batch of images from the flow.
-        """
-        # Sample latent representation from prior
-        if z_init is None:
-            rng, normal_rng = random.split(rng)
-            z = random.normal(normal_rng, shape=img_shape)
-        else:
-            z = z_init
-
-        # Transform z to x by inverting the flows
-        ldj = np.zeros(img_shape[0])
-        for flow in reversed(self.flows):
-            z, ldj, rng = flow(z, ldj, rng, reverse=True)
-        return z, rng
--- sibylla/Norm_Flows/run_NF.py
+++ sibylla/Norm_Flows/run_NF.py
@@ -19,7 +19,7 @@ from urllib.error import HTTPError
 from layers import *
 from DataLoader import DataLoader
 from TrainerModule import TrainerModule
-from flows import ImageFlow
+# from flows import MultiScaleImageFlow
 
 
 def show_imgs(imgs, title=None, row_size=4):
@@ -42,50 +42,6 @@ def show_imgs(imgs, title=None, row_size=4):
     plt.close()
 
 
-def create_checkerboard_mask(h, w, invert=False):
-    x, y = np.arange(h, dtype=np.int32), np.arange(w, dtype=np.int32)
-    xx, yy = np.meshgrid(x, y, indexing='ij')
-    mask = np.fmod(xx + yy, 2)
-    mask = mask.astype(np.float32).reshape(1, h, w, 1)
-    if invert:
-        mask = 1 - mask
-    return mask
-
-def create_channel_mask(c_in, invert=False):
-    mask = np.concatenate([
-                np.ones((c_in//2,), dtype=np.float32),
-                np.zeros((c_in-c_in//2,), dtype=np.float32)
-            ])
-    mask = mask.reshape(1, 1, 1, c_in)
-    if invert:
-        mask = 1 - mask
-    return mask
-
-def create_multiscale_flow():
-    flow_layers = []
-
-    vardeq_layers = [CouplingLayer(network=GatedConvNet(c_out=2, c_hidden=16),
-                                   mask=create_checkerboard_mask(h=28, w=28, invert=(i%2==1)),
-                                   c_in=1) for i in range(4)]
-    flow_layers += [VariationalDequantization(var_flows=vardeq_layers)]
-
-    flow_layers += [CouplingLayer(network=GatedConvNet(c_out=2, c_hidden=32),
-                                  mask=create_checkerboard_mask(h=28, w=28, invert=(i%2==1)),
-                                  c_in=1) for i in range(2)]
-    flow_layers += [SqueezeFlow()]
-    for i in range(2):
-        flow_layers += [CouplingLayer(network=GatedConvNet(c_out=8, c_hidden=48),
-                                      mask=create_channel_mask(c_in=4, invert=(i%2==1)),
-                                      c_in=4)]
-    flow_layers += [SplitFlow(),
-                    SqueezeFlow()]
-    for i in range(4):
-        flow_layers += [CouplingLayer(network=GatedConvNet(c_out=16, c_hidden=64),
-                                      mask=create_channel_mask(c_in=8, invert=(i%2==1)),
-                                      c_in=8)]
-
-    flow_model = ImageFlow(flow_layers)
-    return flow_model
 
 if __name__ == "__main__":
     checkpoint_path = "./saved_models/simple_example"
@@ -125,5 +81,6 @@ if __name__ == "__main__":
 
     print("Creating flow...", end='')
     flow_dict = {"simple": {}, "vardeq": {}, "multiscale": {}}
-    flow_dict["multiscale"]["model"], flow_dict["multiscale"]["result"] = TrainerModule.train_flow(create_multiscale_flow(), checkpoint_path, model_name="MNISTFlow_multiscale")
+    flow = MultiScaleImageFlow()
+    flow_dict["multiscale"]["model"], flow_dict["multiscale"]["result"] = TrainerModule.train_flow(flow, checkpoint_path, model_name="MNISTFlow_multiscale")
     print("Done!")
