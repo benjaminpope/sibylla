248f4d25178fc0399803d453da25fef5e064a23f
Adam Taras <ataras2@gmail.com>
2023-01-17 23:39:34 +1100

    fixing indenting

--- sibylla/Norm_Flows/x_save_flow.py.py
+++ sibylla/Norm_Flows/x_save_flow.py.py
@@ -43,61 +43,61 @@ MNIST_IMAGE_SHAPE = (28, 28, 1)
 def make_conditioner(event_shape: Sequence[int],
                      hidden_sizes: Sequence[int],
                      num_bijector_params: int) -> hk.Sequential:
-  """Creates an MLP conditioner for each layer of the flow."""
-  return hk.Sequential([
-      hk.Flatten(preserve_dims=-len(event_shape)),
-      hk.nets.MLP(hidden_sizes, activate_final=True),
-      # We initialize this linear layer to zero so that the flow is initialized
-      # to the identity function.
-      hk.Linear(
-          np.prod(event_shape) * num_bijector_params,
-          w_init=jnp.zeros,
-          b_init=jnp.zeros),
-      hk.Reshape(tuple(event_shape) + (num_bijector_params,), preserve_dims=-1),
-  ])
+    """Creates an MLP conditioner for each layer of the flow."""
+    return hk.Sequential([
+        hk.Flatten(preserve_dims=-len(event_shape)),
+        hk.nets.MLP(hidden_sizes, activate_final=True),
+        # We initialize this linear layer to zero so that the flow is initialized
+        # to the identity function.
+        hk.Linear(
+            np.prod(event_shape) * num_bijector_params,
+            w_init=jnp.zeros,
+            b_init=jnp.zeros),
+        hk.Reshape(tuple(event_shape) + (num_bijector_params,), preserve_dims=-1),
+    ])
 
 
 def make_flow_model(event_shape: Sequence[int],
                     num_layers: int,
                     hidden_sizes: Sequence[int],
                     num_bins: int) -> distrax.Transformed:
-  """Creates the flow model."""
-  # Alternating binary mask.
-  mask = jnp.arange(0, np.prod(event_shape)) % 2
-  mask = jnp.reshape(mask, event_shape)
-  mask = mask.astype(bool)
+    """Creates the flow model."""
+    # Alternating binary mask.
+    mask = jnp.arange(0, np.prod(event_shape)) % 2
+    mask = jnp.reshape(mask, event_shape)
+    mask = mask.astype(bool)
 
-  def bijector_fn(params: Array):
-    return distrax.RationalQuadraticSpline(
-        params, range_min=0., range_max=1.)
+    def bijector_fn(params: Array):
+        return distrax.RationalQuadraticSpline(
+            params, range_min=0., range_max=1.)
 
-  # Number of parameters for the rational-quadratic spline:
-  # - `num_bins` bin widths
-  # - `num_bins` bin heights
-  # - `num_bins + 1` knot slopes
-  # for a total of `3 * num_bins + 1` parameters.
-  num_bijector_params = 3 * num_bins + 1
+    # Number of parameters for the rational-quadratic spline:
+    # - `num_bins` bin widths
+    # - `num_bins` bin heights
+    # - `num_bins + 1` knot slopes
+    # for a total of `3 * num_bins + 1` parameters.
+    num_bijector_params = 3 * num_bins + 1
 
-  layers = []
-  for _ in range(num_layers):
-    layer = distrax.MaskedCoupling(
-        mask=mask,
-        bijector=bijector_fn,
-        conditioner=make_conditioner(event_shape, hidden_sizes,
-                                     num_bijector_params))
-    layers.append(layer)
-    # Flip the mask after each layer.
-    mask = jnp.logical_not(mask)
+    layers = []
+    for _ in range(num_layers):
+        layer = distrax.MaskedCoupling(
+            mask=mask,
+            bijector=bijector_fn,
+            conditioner=make_conditioner(event_shape, hidden_sizes,
+                                        num_bijector_params))
+        layers.append(layer)
+        # Flip the mask after each layer.
+        mask = jnp.logical_not(mask)
 
-  # We invert the flow so that the `forward` method is called with `log_prob`.
-  flow = distrax.Inverse(distrax.Chain(layers))
-  base_distribution = distrax.Independent(
-      distrax.Uniform(
-          low=jnp.zeros(event_shape),
-          high=jnp.ones(event_shape)),
-      reinterpreted_batch_ndims=len(event_shape))
+    # We invert the flow so that the `forward` method is called with `log_prob`.
+    flow = distrax.Inverse(distrax.Chain(layers))
+    base_distribution = distrax.Independent(
+        distrax.Uniform(
+            low=jnp.zeros(event_shape),
+            high=jnp.ones(event_shape)),
+        reinterpreted_batch_ndims=len(event_shape))
 
-  return distrax.Transformed(base_distribution, flow)
+    return distrax.Transformed(base_distribution, flow)
 
 def prepare_data(batch: Batch, prng_key: Optional[PRNGKey] = None) -> Array:
     data = batch["image"].astype(np.float32)
