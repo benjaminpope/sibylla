d56361e44533ef89a671f8694ab1b7fff7afb9b6
ataras2 <ataras2@gmail.com>
2023-03-20 12:05:00 +1100

    renamed flow config

--- sibylla/Norm_Flows/train_tf.py
+++ sibylla/Norm_Flows/train_tf.py
@@ -31,13 +31,12 @@ import tensorflow_datasets as tfds
 from ModelStorage import ModelStorage
 
 # import all configs
-import simple_flow_config
-import simple_flow_config_v2
+import sibylla.Norm_Flows.uniform_base_flow_config as uniform_base_flow_config
 
 Array = chex.Array
 Numeric = Union[Array, float]
 
-flags.DEFINE_enum('flow_model', 'simple_flow_v2',
+flags.DEFINE_enum('flow_model', 'simple_flow',
                   ['simple_flow', 'simple_flow_v2'], 'Flow to train')
 flags.DEFINE_enum('dataset', 'MNIST',
                   ['MNIST'], 'Dataset to train')
@@ -71,7 +70,7 @@ def load_dataset(split: tfds.Split, batch_size: int) -> Iterator[Batch]:
 
 def main(_):
     if FLAGS.flow_model == "simple_flow":
-        config = simple_flow_config.get_config(FLAGS.dataset)
+        config = uniform_base_flow_config.get_config(FLAGS.dataset)
     elif FLAGS.flow_model == "simple_flow_v2":
         config = simple_flow_config_v2.get_config(FLAGS.dataset)
     else:
