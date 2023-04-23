1909a244f0b0ccfb3a29da6e7f4956e2529bb63b
ataras2 <ataras2@gmail.com>
2023-04-23 11:19:01 +1000

    squeeze layer workaround

---
+++ sibylla/Norm_Flows/x_squeeze_channel_mask.py
@@ -0,0 +1,18 @@
+# experiment to see what happens when masks are squeezed
+
+from Image_masks import *
+from custom_bijectors import Squeeze
+
+starting_mask = ChannelMask((14,14,4))
+
+squeeze_layer = Squeeze(3,3)
+
+final_mask = squeeze_layer.inverse(starting_mask._mask)
+
+final_mask = ImageMask(final_mask)
+
+plt.figure()
+final_mask.visualise()
+plt.show()
+# conclusion: can get away without using squeeze by intelligently constructing ignorance masks such 
+# that each column is ignored
